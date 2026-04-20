95275d1201cdec86b3eb8743f306045bde060c2c
leehseub <leehseub@naver.com>
2026-04-20 17:50:29 +0900

    [CHORE] 관리자 페이지 테스트용 상품 목업 데이터 추가

---
+++ src/mocks/mockProducts.ts
@@ -0,0 +1,74 @@
+import type { Product } from '../types';
+
+export const MOCK_PRODUCTS: Product[] = [
+  {
+    id: 1,
+    product_name: '사과',
+    product_price: 1000,
+    product_category: '식품',
+    product_stock: 50,
+  },
+  {
+    id: 2,
+    product_name: '배',
+    product_price: 1500,
+    product_category: '식품',
+    product_stock: 100,
+  },
+  {
+    id: 3,
+    product_name: '샴푸',
+    product_price: 10000,
+    product_category: '생필품',
+    product_stock: 25,
+  },
+  {
+    id: 4,
+    product_name: '핸드워시',
+    product_price: 5000,
+    product_category: '생필품',
+    product_stock: 30,
+  },
+  {
+    id: 5,
+    product_name: '바디워시',
+    product_price: 7000,
+    product_category: '생필품',
+    product_stock: 70,
+  },
+  {
+    id: 6,
+    product_name: '수건',
+    product_price: 3000,
+    product_category: '생필품',
+    product_stock: 10,
+  },
+  {
+    id: 7,
+    product_name: 'iPhone',
+    product_price: 1000000,
+    product_category: '전자제품',
+    product_stock: 5,
+  },
+  {
+    id: 8,
+    product_name: 'Galaxy',
+    product_price: 1000000,
+    product_category: '전자제품',
+    product_stock: 5,
+  },
+  {
+    id: 9,
+    product_name: 'MacBook',
+    product_price: 2000000,
+    product_category: '전자제품',
+    product_stock: 3,
+  },
+  {
+    id: 10,
+    product_name: '보조 배터리',
+    product_price: 25000,
+    product_category: '전자제품',
+    product_stock: 10,
+  },
+];
